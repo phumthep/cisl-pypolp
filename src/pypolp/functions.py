c693eb157964f81f6783fa6ea9d96f6f5bf55251
Dae <65160408+phumthep@users.noreply.github.com>
2023-12-12 10:42:31 -0500

    Check if subproblems produce int solutions

--- src/pypolp/functions.py
+++ src/pypolp/functions.py
@@ -16,11 +16,44 @@ def get_temp_dir() -> str:
     return os.path.join(get_pypolp_dir(), 'temp')
 
 
-def check_is_binary(
+def check_is_binary_from_df(
+        df: pd.DataFrame,
+        target_varnames: list[str],
+        atol: float = 1e-5,
+        return_non_binary: bool = False
+        ) -> bool:
+    ''' Return true if all target variables are binary.
+    '''
+    # PowNet's variable naming convention is 'variable[node, time]'
+    target_variables = df.copy()
+    target_variables['variable'] = target_variables['name'].str.split('[', expand=True).iloc[:, 0]
+    target_variables = target_variables.loc[target_variables['variable'].isin(target_varnames), :]
+    
+    # To check whether values are binary,
+    # we need to first prevent numerical instability
+    target_variables.loc[
+        np.isclose(target_variables['value'], 0, atol=atol), 'value'
+                   ] = 0
+    target_variables.loc[
+        np.isclose(target_variables['value'], 1, atol=atol), 'value'
+                   ] = 1
+    
+    # Values that are not exactly 0 or 1 are non-binary values
+    non_int_vars = target_variables[target_variables['value'] > 0]
+    non_int_vars = non_int_vars[non_int_vars['value'] < 1]
+    num_non_int = len(non_int_vars)
+    
+    if return_non_binary:
+        return (num_non_int == 0, non_int_vars)
+    else:
+        return num_non_int == 0
+
+
+def check_is_binary_from_model(
         model: gp.Model,
         target_varnames: list[str],
         atol: float = 1e-5,
-        return_non_binary = False
+        return_non_binary: bool = False
         ) -> bool:
     ''' Check if target variables are binary. Return non-binary
     variables otherwise.
@@ -30,7 +63,7 @@ def check_is_binary(
     for v in variables:
         if v.varname.split('[')[0] in target_varnames:
             filtered_vars[v.varname] = v.X
-    
+
     target_variables = pd.DataFrame.from_dict(filtered_vars, orient='index')
     target_variables = target_variables.reset_index()
     target_variables.columns = ['name', 'value']
@@ -53,6 +86,8 @@ def check_is_binary(
         return (num_non_int == 0, non_int_vars)
     else:
         return num_non_int == 0
+    
+
 
 
 def generate_convex_names(n_subproblems: int):
