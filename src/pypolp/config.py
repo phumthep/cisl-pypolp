5945d1b2054f55f010c35e7668793ef34b62a14e
phumthep <phumthep@gmail.com>
2024-04-22 20:03:27 -0400

    Fix file paths

--- src/pypolp/config.py
+++ src/pypolp/config.py
@@ -4,7 +4,7 @@ import os
 from pypolp.functions import get_pypolp_dir
 
 
-config_file = 'user_config.ini'
+config_file = os.path.join(get_pypolp_dir(), "user_config.ini")
 CONFIG = configparser.ConfigParser()
 CONFIG.read(config_file)
 
@@ -13,100 +13,89 @@ def get_config() -> configparser.ConfigParser:
     return CONFIG
 
 
-
-#---- Dantzig-Wolfe
+# ---- Dantzig-Wolfe
 def get_dw_max_iter() -> int:
-    ''' The number of iterations impact the solution quality.
-    '''
-    return CONFIG.getint('DW', 'MAXITER')
+    """The number of iterations impact the solution quality."""
+    return CONFIG.getint("DW", "MAXITER")
 
 
 def get_dw_improve() -> float:
-    ''' Terminate DW when incremental improvement in
+    """Terminate DW when incremental improvement in
     the objective value of the master problem is below a threshold.
-    '''
-    return CONFIG.getfloat('DW', 'DWIMPROVE')
+    """
+    return CONFIG.getfloat("DW", "DWIMPROVE")
 
 
 def get_dw_rmpgap() -> float:
-    ''' Terminate DW when the gap between the current objval of the RMP
+    """Terminate DW when the gap between the current objval of the RMP
     and the current lower bound is larger than a threshold.
-    '''
-    return CONFIG.getfloat('DW', 'DWRMPGAP')
+    """
+    return CONFIG.getfloat("DW", "DWRMPGAP")
 
 
 def get_dw_recover_integer() -> bool:
-    ''' If we need to reoptimize the master problem with binary variables.
-    '''
-    return CONFIG.getboolean('DW', 'RECOVER_INTEGER')
+    """If we need to reoptimize the master problem with binary variables."""
+    return CONFIG.getboolean("DW", "RECOVER_INTEGER")
 
 
 def get_dw_verbose() -> bool:
-    ''' Print new extreme points/rays added at each iteration.
-    '''
-    return CONFIG.getboolean('DW', 'VERBOSE')
+    """Print new extreme points/rays added at each iteration."""
+    return CONFIG.getboolean("DW", "VERBOSE")
 
 
-
-#---- Master Problem
+# ---- Master Problem
 def get_master_verbose() -> bool:
-    ''' Print Gurobi log for the master problem.
-    '''
-    return CONFIG.getboolean('DWMASTER', 'VERBOSE')
+    """Print Gurobi log for the master problem."""
+    return CONFIG.getboolean("DWMASTER", "VERBOSE")
 
 
-def  get_master_timelimit() -> float:
-    ''' The time to solve the master problem depends on 
-    the number of proposals so far. 
-    '''
-    return CONFIG.getfloat('DWMASTER', 'TIMELIMIT')
+def get_master_timelimit() -> float:
+    """The time to solve the master problem depends on
+    the number of proposals so far.
+    """
+    return CONFIG.getfloat("DWMASTER", "TIMELIMIT")
 
 
 def get_master_mipgap() -> float:
-    ''' Specify mipgap of the master problem.
-    '''
-    return CONFIG.getfloat('DWMASTER', 'MIPGAP')
+    """Specify mipgap of the master problem."""
+    return CONFIG.getfloat("DWMASTER", "MIPGAP")
 
 
-
-#---- Subproblems
+# ---- Subproblems
 def get_subp_warmstart() -> bool:
-    return CONFIG.getboolean('DWSUBPROBLEM', 'WARMSTART')
+    return CONFIG.getboolean("DWSUBPROBLEM", "WARMSTART")
+
 
 def get_subp_verbose() -> bool:
-    ''' Print Gurobi log for subproblems.
-    '''
-    return CONFIG.getboolean('DWSUBPROBLEM', 'VERBOSE')
+    """Print Gurobi log for subproblems."""
+    return CONFIG.getboolean("DWSUBPROBLEM", "VERBOSE")
 
 
 def get_subp_mipgap() -> bool:
-    ''' Specify mipgap of all subproblems.
-    '''
-    return CONFIG.getfloat('DWSUBPROBLEM', 'MIPGAP')
+    """Specify mipgap of all subproblems."""
+    return CONFIG.getfloat("DWSUBPROBLEM", "MIPGAP")
 
 
 def get_subp_relax() -> bool:
-    ''' Specify whether to convert the subproblems to linear programs 
+    """Specify whether to convert the subproblems to linear programs
     by relaxing the integer variables.
-    '''
-    return CONFIG.getboolean('DWSUBPROBLEM', 'RELAX')
+    """
+    return CONFIG.getboolean("DWSUBPROBLEM", "RELAX")
 
 
-
-#---- Gurobi
+# ---- Gurobi
 def get_gp_warmstart() -> bool:
-    return CONFIG.getboolean('GUROBI', 'WARMSTART')
+    return CONFIG.getboolean("GUROBI", "WARMSTART")
 
 
 def get_gp_mipgap() -> float:
-    return CONFIG.getfloat('GUROBI', 'MIPGAP')
+    return CONFIG.getfloat("GUROBI", "MIPGAP")
 
 
 def get_gp_verbose() -> bool:
-    return CONFIG.getboolean('GUROBI', 'VERBOSE')
+    return CONFIG.getboolean("GUROBI", "VERBOSE")
 
 
 def get_gp_record() -> bool:
-    ''' If the GurobipyOptimizer should keep track of statistics.
-    '''
-    return CONFIG.getboolean('GUROBI', 'RECORD')
+    """If the GurobipyOptimizer should keep track of statistics."""
+    return CONFIG.getboolean("GUROBI", "RECORD")
--- src/pypolp/functions.py
+++ src/pypolp/functions.py
@@ -6,78 +6,80 @@ import pandas as pd
 
 
 def get_pypolp_dir() -> str:
-    return os.path.dirname(
-        os.path.dirname(
-            os.path.dirname(
-                os.path.realpath(__file__))))
+    local_dir = os.getcwd()
+    return os.path.dirname(local_dir)
 
 
 def get_temp_dir() -> str:
-    return os.path.join(get_pypolp_dir(), 'temp',)
+    return os.path.join(
+        get_pypolp_dir(),
+        "temp",
+    )
 
 
 def get_non_binary_from_df(
-        df: pd.DataFrame,
-        target_varnames: list[str],
-        atol: float = 1e-5,
+    df: pd.DataFrame,
+    target_varnames: list[str],
+    atol: float = 1e-5,
 ) -> bool:
-    ''' Return true if all target variables are binary.
-    '''
+    """Return true if all target variables are binary."""
     # PowNet's variable naming convention is 'variable[node, time]'
     target_variables = df.copy()
-    target_variables['variable'] = target_variables['name'].str.split(
-        '[', expand=True).iloc[:, 0]
-    target_variables = target_variables.loc[target_variables['variable'].isin(
-        target_varnames), :]
+    target_variables["variable"] = (
+        target_variables["name"].str.split("[", expand=True).iloc[:, 0]
+    )
+    target_variables = target_variables.loc[
+        target_variables["variable"].isin(target_varnames), :
+    ]
 
     # To check whether values are binary,
     # we need to first prevent numerical instability
     target_variables.loc[
-        np.isclose(target_variables['value'], 0, atol=atol), 'value'
+        np.isclose(target_variables["value"], 0, atol=atol), "value"
     ] = 0
     target_variables.loc[
-        np.isclose(target_variables['value'], 1, atol=atol), 'value'
+        np.isclose(target_variables["value"], 1, atol=atol), "value"
     ] = 1
 
     # Values that are not exactly 0 or 1 are non-binary values
-    non_bin_vars = target_variables[target_variables['value'] > 0]
-    non_bin_vars = non_bin_vars[non_bin_vars['value'] < 1]
+    non_bin_vars = target_variables[target_variables["value"] > 0]
+    non_bin_vars = non_bin_vars[non_bin_vars["value"] < 1]
 
     return non_bin_vars
 
 
 def get_non_binary_from_model(
-        model: gp.Model,
-        target_varnames: list[str],
-        atol: float = 1e-5,
+    model: gp.Model,
+    target_varnames: list[str],
+    atol: float = 1e-5,
 ) -> pd.DataFrame:
-    ''' Check if target variables are binary. Return non-binary
+    """Check if target variables are binary. Return non-binary
     variables otherwise.
-    '''
+    """
     filtered_vars = {}
     for v in model.getVars():
         # Match with the variable types in target_varnames
         # The full variable name is in the format 'variable[node, time]'
         # or just 'variable[time]
-        if v.varname.split('[')[0] in target_varnames:
+        if v.varname.split("[")[0] in target_varnames:
             filtered_vars[v.varname] = v.X
 
-    target_variables = pd.DataFrame.from_dict(filtered_vars, orient='index')
+    target_variables = pd.DataFrame.from_dict(filtered_vars, orient="index")
     target_variables = target_variables.reset_index()
-    target_variables.columns = ['name', 'value']
+    target_variables.columns = ["name", "value"]
 
     # To check whether values are binary,
     # we need to first prevent numerical instability
     target_variables.loc[
-        np.isclose(target_variables['value'], 0, atol=atol), 'value'
+        np.isclose(target_variables["value"], 0, atol=atol), "value"
     ] = 0
     target_variables.loc[
-        np.isclose(target_variables['value'], 1, atol=atol), 'value'
+        np.isclose(target_variables["value"], 1, atol=atol), "value"
     ] = 1
 
     # Values that are not exactly 0 or 1 are non-binary values
-    non_bin_vars = target_variables[target_variables['value'] > 0]
-    non_bin_vars = non_bin_vars[non_bin_vars['value'] < 1]
+    non_bin_vars = target_variables[target_variables["value"] > 0]
+    non_bin_vars = non_bin_vars[non_bin_vars["value"] < 1]
 
     return non_bin_vars
 
@@ -92,19 +94,19 @@ def count_mps_files(instance_folder: str) -> int:
 
 
 def generate_convex_names(n_subproblems: int):
-    ''' The name of convexity contrainsts are in the format convex_[block_id].
+    """The name of convexity contrainsts are in the format convex_[block_id].
     We recover the final solution using this regex pattern.
-    '''
-    return [f'convex_{j}' for j in range(n_subproblems)]
+    """
+    return [f"convex_{j}" for j in range(n_subproblems)]
 
 
 def separate_master_vars(master_vars: pd.DataFrame) -> tuple[pd.DataFrame]:
-    ''' Return a dataframe with only master-only variables and a dataframe
+    """Return a dataframe with only master-only variables and a dataframe
     with weighting variables, respectively.
-    '''
-    master_only_vars = master_vars[~master_vars['variable'].str.contains(
-        'B\(', regex=True)]
-    master_only_vars = master_only_vars.set_index('variable')
-    betas = master_vars[master_vars['variable'].str.contains(
-        'B\(', regex=True)].copy()
+    """
+    master_only_vars = master_vars[
+        ~master_vars["variable"].str.contains("B\(", regex=True)
+    ]
+    master_only_vars = master_only_vars.set_index("variable")
+    betas = master_vars[master_vars["variable"].str.contains("B\(", regex=True)].copy()
     return master_only_vars, betas
